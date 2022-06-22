bc465e2dc585a383fdf670de2bca2df97495bf66
Tami <fire17@gmail.com>
2022-06-22 21:52:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# https://github.com/fire17/xo-heroku/edit/main/app.py
+
 from xo import *
 from flask import Flask
 
@@ -9,15 +11,20 @@ def my_view_func(text):
         if len(text.split("/")[1])>0:
             key = text.split("/")[1]
             if xo.wait[key].value() == None:
-                xo.wait[key] = False
+                xo.wait[key] = "awaiting "+key+"..."
+                xo.wait[key].done = False
             if len(text.split("/")) > 2 and len(text.split("/")[2])>0:
                 value = text.split("/")[2]
+                if len(text.split("/")) > 3 and len(text.split("/")[3])>0:
+                    msg = text.split("/")[3]
+                    xo.wait[key] = msg
                 if "1" in value or "true" in value.lower():
-                    xo.wait[key] = True
-                else:
-                    xo.wait[key] = False
-            return "@@@"+str(xo.wait[key].value())+"@@@"
-    return "!!!"+text+"!!!"
+                    xo.wait[key].done = True
+                elif "0" in value or "false" in value.lower():
+                    xo.wait[key].done = False
+            res = xo.wait[key].done.value() == True
+            return "@"+res+str(xo.wait[key].value())+"@@@"
+    return "@1"+text+"!!!"
 @app.route('/')
 def hello():
     return 'Hello, World!'
