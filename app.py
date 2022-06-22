fbe04ea772601b10b2dc4f0968c4ef4d210b9e92
Tami <fire17@gmail.com>
2022-06-22 22:05:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ app = Flask(__name__)
 
 @app.route('/<path:text>')
 def my_view_func(text):
+    print("text :::",text)
     if text.startswith("await/"):
         if len(text.split("/")[1])>0:
             key = text.split("/")[1]
+            print("key :::",key)
             if xo.wait[key].value() == None:
                 xo.wait[key] = "awaiting "+key+"..."
                 xo.wait[key].done = False
             if len(text.split("/")) > 2 and len(text.split("/")[2])>0:
                 value = text.split("/")[2]
+                print("value :::",value)
                 if len(text.split("/")) > 3 and len(text.split("/")[3])>0:
                     msg = text.split("/")[3]
+                    print("value :::",msg)
                     xo.wait[key] = msg
                 if "1" in value or "true" in value.lower():
                     xo.wait[key].done = True
@@ -25,8 +29,13 @@ def my_view_func(text):
             res = "0"
             if xo.wait[key].done.value() == True:
                 res = "1"
-            return "@"+res+str(xo.wait[key].value())+"@@@"
-    return "@1"+text+"!!!"
+            final = "@"+res+str(xo.wait[key].value())
+            print("value :::",msg)
+            print()
+            return final
+    print()
+    return "@1"+text+"!!!echo!!!"
+    
 @app.route('/')
 def hello():
     return 'Hello, World!'
