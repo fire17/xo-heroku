b0f3c63d0cada42674b380b16103d58f1b1d8cc6
Tami <fire17@gmail.com>
2022-06-22 17:12:27 +0300

    auto set await child to False

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ def my_view_func(text):
     if text.startswith("await/"):
         if len(text.split("/")[1])>0:
             key = text.split("/")[1]
+            if xo.await[key].value() == None:
+                xo.await[key] = False
             if len(text.split("/")) > 2 and len(text.split("/")[2])>0:
                 value = text.split("/")[2]
                 if "1" in value or "true" in value.lower():
                     xo.await[key] = True
                 else:
                     xo.await[key] = False
-    return "!!!"+str(xo.await[key].value)+"!!!"
+    return "!!!"+str(xo.await[key].value())+"!!!"
   
 @app.route('/')
 def hello():
